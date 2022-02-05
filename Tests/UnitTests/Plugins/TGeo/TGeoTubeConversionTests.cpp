e8d7c1f73f6efc915271bcf9573a9497b875ae40
Sakib Rahman <rahmans@narval1.narval.calcul.quebec>
2022-02-04 21:21:32 -0500

    Add test parameters for normals to TGeoCtub object (conversion to disc surface)

--- Tests/UnitTests/Plugins/TGeo/TGeoTubeConversionTests.cpp
+++ Tests/UnitTests/Plugins/TGeo/TGeoTubeConversionTests.cpp
@@ -193,6 +193,13 @@ BOOST_AUTO_TEST_CASE(TGeoTube_to_DiscSurface) {
   double hz = 2.;
   double phimin = -45.;
   double phimax = 45.;
+  double nxlow = 0;
+  double nylow = 0.707;
+  double nzlow = -0.707;
+  double nxhi = 0.;
+  double nyhi = 0.707;
+  double nzhi = 0.707;
+
 
   new TGeoManager("trd1", "poza9");
   TGeoMaterial *mat = new TGeoMaterial("Al", 26.98, 13, 2.7);
